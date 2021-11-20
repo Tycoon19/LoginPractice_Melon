9eeb933c59d7aec18768670d40184ea0c7794b78
Tycoon19 <kevin960831@naver.com>
2021-11-20 16:24:18 +0900

    로그인 구현

--- app/src/main/java/com/example/loginpractice/MainActivity.kt
+++ app/src/main/java/com/example/loginpractice/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.loginpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val getId = idContent.text.toString()
+            val getPw = pwContent.text.toString()
+            
+            if (getId == "kevin960831@naver.com" && getPw == "jae0831"){
+                Toast.makeText(this, "관리자 입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "아이디와 비밀번호를 다시 확인해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
