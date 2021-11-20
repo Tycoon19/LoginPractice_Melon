0185936a4dc82a05cf9bfafae76c3b8c3b13bc8c
Tycoon19 <kevin960831@naver.com>
2021-11-20 16:36:52 +0900

    코드에 적은 관리자 이름도 변수에 나오게 - String 가공하기

--- app/src/main/java/com/example/loginpractice/MainActivity.kt
+++ app/src/main/java/com/example/loginpractice/MainActivity.kt
@@ -14,9 +14,11 @@ class MainActivity : AppCompatActivity() {
 
             val getId = idContent.text.toString()
             val getPw = pwContent.text.toString()
-            
+
+            val name = "재휘"
+
             if (getId == "kevin960831@naver.com" && getPw == "jae0831"){
-                Toast.makeText(this, "관리자 입니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "${name} 관리자 입니다.", Toast.LENGTH_SHORT).show()
             }
             else{
                 Toast.makeText(this, "아이디와 비밀번호를 다시 확인해주세요", Toast.LENGTH_SHORT).show()
